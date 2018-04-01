6bacb75643f837a975fdf876163e7cfdf1c4e616
nnuch <Noochie@nnsmac.local>
2018-04-01 13:55:37 -0400

    create new home tab

---
+++ iOS-Assignment-3/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  iOS-Assignment-3
+//
+//  Created by Nuch Phromsorn on 2018-04-01.
+//  Copyright © 2018 Nuch Phromsorn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface HomeViewController : ViewController
+
+@end
---
+++ iOS-Assignment-3/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  iOS-Assignment-3
+//
+//  Created by Nuch Phromsorn on 2018-04-01.
+//  Copyright © 2018 Nuch Phromsorn. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.tabBarController setSelectedIndex:0];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
